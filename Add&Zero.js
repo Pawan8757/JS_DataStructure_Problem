6b636f1061495f69da00d6266a8a12feecc75ec6
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-05-20 12:10:12 +0530

    adding three integer whose sum to be zero

---
+++ Add&Zero.js
@@ -0,0 +1,9 @@
+var arr=[4,4,-8]
+
+for(var i=0;i<=arr.length;i++)
+{
+    console.log(arr[i])
+}
+var sum=arr[0]+arr[1]+arr[2];
+console.log("Sum of three integer = "+sum);
+console.log("Sum of this integer adds to Zero");
